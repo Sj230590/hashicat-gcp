32d72d0d9d1fe14c5ef7a169ea6c837af0243877
Saurabh <sj230590@gmail.com>
2021-05-18 11:47:10 +0000

    Added vpc module

--- remote_backend.tf
+++ remote_backend.tf
@@ -4,7 +4,7 @@
 
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "sj23-training"
     workspaces {
       name = "hashicat-gcp"
